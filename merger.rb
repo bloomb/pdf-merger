38c157be6dbe0dbe42589a45d685a740868b25db
Ben Bloom <bbloom22@gmail.com>
2015-10-20 14:00:20 -0400

    Fix merger when a combine file already exists

--- merger.rb
+++ merger.rb
@@ -7,4 +7,15 @@ Dir.glob("pdfs/*.pdf") {|file|
   pdf << CombinePDF.load(file) # one way to combine, very fast.
 }
 
-pdf.save "combined.pdf"
+file_name = "combined.pdf"
+if File.exist?(file_name)
+  i=1
+  file_name = 'combined' + i.to_s + '.pdf'
+  until !(File.exist?(file_name))
+    i+=1
+    file_name = 'combined' + i.to_s + '.pdf'
+  end
+  pdf.save file_name
+else
+  pdf.save "combined.pdf"
+end
