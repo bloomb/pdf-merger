2bd9335e29ebf2e577e1174b54e6682586d555bb
Ben Bloom <bbloom22@gmail.com>
2016-03-03 14:48:32 -0500

    Update save location

--- merger.rb
+++ merger.rb
@@ -7,15 +7,15 @@ Dir.glob("pdfs/*.pdf") {|file|
   pdf << CombinePDF.load(file) # one way to combine, very fast.
 }
 
-file_name = "combined.pdf"
+file_name = "merged_pdfs/combined.pdf"
 if File.exist?(file_name)
   i=1
-  file_name = 'combined' + i.to_s + '.pdf'
+  file_name = 'merged_pdfs/combined' + i.to_s + '.pdf'
   until !(File.exist?(file_name))
     i+=1
-    file_name = 'combined' + i.to_s + '.pdf'
+    file_name = 'merged_pdfs/combined' + i.to_s + '.pdf'
   end
   pdf.save file_name
 else
-  pdf.save "combined.pdf"
+  pdf.save file_name
 end
